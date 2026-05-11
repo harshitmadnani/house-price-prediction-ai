8cc7eafe1bbb8813b78c55be20f57e36879a0e22
Harshit Madnani <hmadnani4@gmail.com>
2026-05-11 13:27:01 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -74,7 +74,7 @@ export default function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://house-price-prediction-ai-1.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
