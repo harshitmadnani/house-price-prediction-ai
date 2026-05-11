20d64762bef32b0f98f0fc641613f52b8665a4de
Harshit Madnani <hmadnani4@gmail.com>
2026-05-11 13:37:53 +0530

    Initial commit

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+allow_origins=[
+    "http://localhost:5173",
+    "https://house-price-prediction-ai-xi.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
